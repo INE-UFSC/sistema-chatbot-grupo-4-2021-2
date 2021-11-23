98cf46f8479c387d132297afd76926f32cb43d84
João Victor S. P <victoronstudy@gmail.com>
2021-11-23 12:00:55 -0300

    Atualiza importação da classe BotCansado

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 #encoding: utf-8
 from SistemaChatBot import SistemaChatBot as scb
-from Bots.BotZangado import BotZangado
+from Bots.BotCansado import BotCansado
 
 ###construa a lista de bots disponíveis aqui
-lista_bots = [BotZangado("Yoda")]
+lista_bots = [BotCansado("Yoda")]
 
 sys = scb.SistemaChatBot("CrazyBots",lista_bots)
 sys.inicio()

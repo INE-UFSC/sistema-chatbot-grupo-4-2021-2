44a341de0d35ac21d22173be5ffe22a92966ed96
joao victor <jvv2003@gmail.com>
2021-11-30 11:47:44 -0300

    bot legal

--- Bots/BotLegal.py
+++ Bots/BotLegal.py
@@ -3,6 +3,7 @@ from Bots.Bot import Bot
 class BotLegal(Bot):
     def __init__(self, nome):
         self.__nome = nome
+        self.comandos = {}
 
     @property
     def nome(self):
@@ -16,10 +17,31 @@ class BotLegal(Bot):
         return "Olá! Eu sou o Bot Legal, podemos ser amigos?"
  
     def mostra_comandos(self):
-        pass
+        self.comandos = {
+                "1": "Apresentar",
+                "2": "Boas vindas",
+                "3": "Mostrar despedida"
+                }
+        numero_de_comandos = len(self.comandos)
+        mensagem = ""
+        for numero, texto in self.comandos.items():
+            mensagem += f"{numero} - {texto}"
+            tem_proxima_linha = numero != numero_de_comandos
+            if tem_proxima_linha:
+                mensagem += "\n"
+        return(mensagem)
     
     def executa_comando(self,cmd):
-        pass
+        cmd = int(cmd)
+        if cmd <= len(self.comandos):
+            if cmd == 1:
+                return self.apresentacao()
+            elif cmd == 2:
+                return self.boas_vindas()
+            elif cmd == 3:
+                return self.despedida()
+        else:
+            return "comando invalido"
 
     def boas_vindas(self):
         return "Oii, que bom que voçê me escolheu, acho que já somos amigos então"
